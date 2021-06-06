6134d2d5da7a6b77740931b2da229413b022db2b
Justin Parrondo <justinparrondo@gmail.com>
2021-06-06 13:06:41 -0400

    Change format and order of questions and change format of output

--- src/main/java/org/cop3330/App.java
+++ src/main/java/org/cop3330/App.java
@@ -11,25 +11,28 @@ public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("What is your weight (in pounds)? ");
-        double weight = in.nextDouble();
-        in.nextLine();
-        System.out.print("What is your gender? (male/female) ");
-        String gender = in.nextLine().toLowerCase();
-        System.out.print("What is the total amount of alcohol you have consumed (in ounces)? ");
+        System.out.print("Enter a 1 if you are male or a 2 if you are female: ");
+        int gender = in.nextInt();
+
+        System.out.print("How many ounces of alcohol did you have? ");
         double alcoholConsumed = in.nextDouble();
-        System.out.print("How long has it been since your last drink? (in hours)? ");
+
+        System.out.print("What is your weight, in pounds? ");
+        double weight = in.nextDouble();
+
+        System.out.print("How many hours has it been since your last drink? ");
         double hours = in.nextDouble();
 
         double alcoholDistributionRatio = 0;
-        if (gender.equals("male"))
+        if (gender == 1)
             alcoholDistributionRatio = 0.73;
-        else if (gender.equals("female"))
+        else if (gender == 2)
             alcoholDistributionRatio = 0.66;
 
         double BAC = (alcoholConsumed * 5.14 / weight * alcoholDistributionRatio) - 0.015 * hours;
 
-        System.out.printf("Your BAC is %.2f\n", BAC);
+        System.out.println();
+        System.out.printf("Your BAC is %.6f\n", BAC);
         System.out.println(BAC < 0.08 ? "It is legal for you to drive." : "It is not legal for you to drive.");
     }
 }
